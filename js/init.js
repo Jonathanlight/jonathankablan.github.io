7c86077756f1499b56eaa870688c2591d0903fc1
jonathan.kablan <jonathan.kablan@gmail.com>
2017-08-08 21:25:12 +0200

    nouvelle version portfolio

---
+++ js/init.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+	 $('.slider').slider({full_width: true});
+     $('.chips').material_chip();
+     $('.chips-initial').material_chip('data');
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
