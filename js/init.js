498473c566f1c3a9bbf0c045b13ed87274fb1ea0
jonathan.kablan <jonathan.kablan@gmail.com>
2017-08-09 11:10:01 +0200

    update site web

--- js/init.js
+++ js/init.js
@@ -1,11 +1,12 @@
-(function($){
-  $(function(){
-
+$(document).ready(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-	 $('.slider').slider({full_width: true});
-     $('.chips').material_chip();
-     $('.chips-initial').material_chip('data');
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    $('.modal').modal();
+    $('#modal1').modal('open');
+    $('#modal1').modal('close');
+
+	$('.slider').slider({full_width: true});
+    $('.chips').material_chip();
+    $('.chips-initial').material_chip('data');
+});
